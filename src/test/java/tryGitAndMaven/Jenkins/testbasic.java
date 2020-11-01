9b646e992e82becc917420f5873eb094b9824d30
Lakshmikanth Katarki <21529@ITCINFOTECH.COM>
2020-11-01 22:03:17 +0530

    added my first maven  project in GIT

---
+++ src/test/java/tryGitAndMaven/Jenkins/testbasic.java
@@ -0,0 +1,11 @@
+package tryGitAndMaven.Jenkins;
+
+import org.testng.annotations.Test;
+
+public class testbasic {
+
+	@Test
+	public void basic(){
+		System.out.println("Hello From Maven and Git integration");
+	}
+}
